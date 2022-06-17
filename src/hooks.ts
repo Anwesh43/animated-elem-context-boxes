11673118f242fa2cb43efecea05659ab9f2a38cd
Anwesh43 <anweshthecool0@gmail.com>
2022-06-17 13:39:04 +0530

    adding useStyle hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { CSSProperties, useEffect, useState } from "react"
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -36,12 +36,69 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w, 
         h
+    }
+}
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+const useStyle = (w : number, h : number, scale : number) => {
+    const position = "absolute"
+    const size = Math.min(w, h) / 10
+    const sf : number = sinify(scale)
+    return {
+        blockStyle() : CSSProperties {
+            const top = `${h / 2 - size / 2}px`
+            const left = `${(w / 2 - size / 2) * sf}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const backColor : string = "indigo"
+            return {
+                position, 
+                top, 
+                left, 
+                width, 
+                height, 
+                background: backColor 
+            }
+        },
+        circleStyle() : CSSProperties {
+            const top = `${(h / 2 - size * 1.5) * sf}px`
+            const left = `${(w / 2 - size / 2)}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const backColor : string = "indigo"
+            const borderRadius = "50%"
+            return {
+                position, 
+                top, 
+                left, 
+                width, 
+                height, 
+                background: backColor,
+                borderRadius 
+            }
+        },
+        lineStyle() : CSSProperties {
+            const top = `${h / 2}px`
+            const left = `${0.75 * w}px`
+            const width = `${size}px`
+            const height = `${size / 10}px`
+            const transform = `rotate(${90 * sf}deg)`
+            return {
+                position, 
+                top, 
+                left,
+                width, 
+                height, 
+                transform 
+            }
+        }
     }
 }
